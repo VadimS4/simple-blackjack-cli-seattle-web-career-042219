94c086b635ee43a28f2f12defc774dbbb540250b
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-10 23:35:49 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,15 +35,6 @@ end
 def hit?(card_total)
   prompt_user
   get_user_input
-  
-  if get_user_input == "s"
-    return card_total
-  elsif get_user_input == "h"
-    deal_card
-    card_total = card_total + card
-  else
-    invalid_command
-  end
 end
 
 def invalid_command
