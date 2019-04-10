ff22eca2ebd8789a950dabce817a92067441776d
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-10 23:29:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,12 +25,11 @@ def end_game(card_total)
 end
 
 def initial_round
-    card_1 = deal_card
-    card_2 = deal_card
-    card_total = card_1 + card_2
-    display_card_total(card_total)
-    return card_total
-  end
+  card_1 = deal_card
+  card_2 = deal_card
+  card_total = card_1 + card_2
+  display_card_total(card_total)
+  return card_total
 end
 
 def hit?(card_total)
